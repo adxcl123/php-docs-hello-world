7a410ec0be7547135561f060798070b007219358
Aby <150267931+adxcl123@users.noreply.github.com>
2025-05-11 03:37:39 -0600

    prueba3

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
 
-<<<<<<< HEAD
-echo "Hello World!";
-=======
-echo "Hola a todos y todas, que tengan un excelente día";
->>>>>>> 557be7186f00150acdd00dc84993fa387b36bdcc
-echo "Hola mundo 21051416 "
+echo "Hola mundo 21051416";
