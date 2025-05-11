b756ac4d2f76b101194254890503457729021b0f
Aby <150267931+adxcl123@users.noreply.github.com>
2025-05-11 03:35:39 -0600

    prueba2

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
+<<<<<<< HEAD
+echo "Hello World!";
+=======
 echo "Hola a todos y todas, que tengan un excelente día";
+>>>>>>> 557be7186f00150acdd00dc84993fa387b36bdcc
+echo "Hola mundo 21051416 "
